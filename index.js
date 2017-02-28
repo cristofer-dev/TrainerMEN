582f352480ce5a2c723ba954e3bebe5a1587b490
Cristofer <cristofer@izit.cl>
2017-02-27 23:57:19 -0300

    Parseando parametros del POST
    	modified:   index.js

--- index.js
+++ index.js
@@ -17,4 +17,11 @@ app.get("/login",function(req,res) {
 	res.render("login")
 })
 
+app.post("/users",function(req,res) {
+	console.log(req.body.email);
+	console.log(req.body.password);
+	res.send("User y Pass Recibidos");
+})
+
+
 app.listen(8080);
