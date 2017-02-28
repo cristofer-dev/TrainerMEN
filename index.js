1053f1a61ba7d777f9b42a9592eefeb2ff7d0ec5
Cristofer <cristofer@izit.cl>
2017-02-27 23:47:27 -0300

    SETEANDO bodyParse
    	modified:   index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+// MONTANDO MODULOS (middlewares)
 app.use(express.static('assets'));
+app.use(bodyParser.json()); // Para poder leer JSON
+app.use(bodyParser.urlencoded({extended: true})); // Para poder leer Parametros
 
 app.set("view engine", "pug");
 
